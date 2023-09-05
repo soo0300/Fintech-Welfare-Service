55286f902a749a521f824e37940ccf29e447e107
김수진 <soo0300@naver.com>
2023-09-05 16:44:03 +0900

    ✨feat: project/mysql 연동

--- src/main/java/com/dream/domain/user/User.java
+++ src/main/java/com/dream/domain/user/User.java
@@ -1,4 +1,23 @@
 package com.dream.domain.user;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
 }
