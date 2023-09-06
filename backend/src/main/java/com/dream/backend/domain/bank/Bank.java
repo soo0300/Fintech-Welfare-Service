b4b5b21f9875fbb75e5487f569d951cc5553b8d7
김수진 <soo0300@naver.com>
2023-09-06 15:34:26 +0900

    ✨feat: 계좌 관련 도메인 설계

--- backend/src/main/java/com/dream/backend/domain/bank/Bank.java
+++ backend/src/main/java/com/dream/backend/domain/bank/Bank.java
@@ -1,4 +1,24 @@
 package com.dream.backend.domain.bank;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bank {
+    @Id
+    @GeneratedValue()
+    @Column(name=" bank_code")
+    private int bankCode;
+
+    @Column(nullable = false, length = 50)
+    private String bankName;
+
 }
