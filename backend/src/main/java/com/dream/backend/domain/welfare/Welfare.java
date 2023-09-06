e717de84a9325b2ed52a83570fd65b22d0860c43
김수진 <soo0300@naver.com>
2023-09-06 11:57:33 +0900

    ✨feat: 복지 정보 엔티티 작성

--- backend/src/main/java/com/dream/backend/domain/welfare/Welfare.java
+++ backend/src/main/java/com/dream/backend/domain/welfare/Welfare.java
@@ -1,4 +1,55 @@
 package com.dream.backend.domain.welfare;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Welfare {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="welfare_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String organization;
+
+    @Column
+    private LocalDateTime start_date;
+
+    @Column
+    private LocalDateTime end_date;
+
+    @Column
+    private String route;
+
+    @Column
+    private String submission;
+
+    @Column
+    private int support_fund;
+
+    @Column
+    private String description_origin;
+
+    @Column
+    private String description;
+
+    @Column
+    private String poster;
+
+    @Column
+    private String img;
+
+    @Column
+    private int support_period;
+
 }
