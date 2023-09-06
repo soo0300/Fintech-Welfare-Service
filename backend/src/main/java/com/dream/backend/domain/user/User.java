af6846db0b0243893f8af4e6e9b173b19830a8b7
김수진 <soo0300@naver.com>
2023-09-06 10:10:12 +0900

    ✨feat: 사용자 엔티티 클래스 작성

--- backend/src/main/java/com/dream/backend/domain/user/User.java
+++ backend/src/main/java/com/dream/backend/domain/user/User.java
@@ -3,12 +3,15 @@ package com.dream.backend.domain.user;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
@@ -16,13 +19,41 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(unique = true, nullable = false, length = 20)
+    @Column(nullable = false, length = 20, updatable = false)
     private String name;
 
-    @Column(unique = true, nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String email;
 
-    @Column(unique = true, nullable = false, length = 100)
-    private String email2;
+    @Column(nullable = false, length = 225)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private int residence_info;
+
+    @Column(nullable = true)
+    private LocalDateTime end_date;
+
+    @Column(nullable = false)
+    private boolean is_ended;
+
+    @Column(nullable = true, length = 50)
+    private String address_1;
+
+    @Column(nullable = true, length = 50)
+    private String address_2;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_date;
+
+    @Column(nullable = true)
+    private int pre_fund;
+
+    @Column(nullable = true)
+    private int total_fund;
+
+    @Column(nullable = true)
+    private int account;
 
 }
