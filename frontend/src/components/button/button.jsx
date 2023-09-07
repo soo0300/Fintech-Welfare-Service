0f11642eed788d63d6d42c27dcfd0359a7245cd0
홍주영 <darkssg@naver.com>
2023-09-07 09:03:37 +0900

    ✨feat: 버튼 기능 추가 및 svg파일 추가

--- frontend/src/components/button/button.jsx
+++ frontend/src/components/button/button.jsx
@@ -7,6 +7,12 @@ const StyledButton = styled(motion.button)`
     props.type !== "icon" && !props.width ? "200px" : props.width};
   height: ${(props) =>
     props.type !== "icon" && !props.height ? "50px" : props.height};
+  background: ${(props) =>
+    props.background ? `var(--${props.background})` : `var(--vanilla-cream)`};
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => (props.color ? `var(--${props.color})` : `var(--white)`)};
+  font-weight: ${(props) => props.weight};
+  border-radius: ${(props) => (props.border ? props.border : "30px")};
   cursor: pointer;
   display: flex;
   justify-content: center;
@@ -15,20 +21,42 @@ const StyledButton = styled(motion.button)`
   object-fit: ${(props) => (props.type === "icon" ? "fill" : "none")};
   padding: ${(props) => (props.type === "icon" ? "0" : props.padding)};
   margin: ${(props) => props.margin};
+  border: none;
 `;
 const Button = (props) => {
-  const { width, height, text, padding, margin, type, plus } = props;
+  const {
+    width,
+    height,
+    background,
+    fontSize,
+    color,
+    weight,
+    border,
+    children,
+    text,
+    padding,
+    margin,
+    type,
+    plus,
+  } = props;
 
   return (
     <StyledButton
       width={width}
       height={height}
+      background={background}
+      fontSize={fontSize}
+      color={color}
+      weight={weight}
+      border={border}
+      children={children}
       padding={padding}
       margin={margin}
       type={type}
       plus={plus}
     >
       {text}
+      {children}
     </StyledButton>
   );
 };
