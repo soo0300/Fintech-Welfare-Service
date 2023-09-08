3c6f97fd84117e1e1a2d3a720debf9d0e5859ac0
김수진 <soo0300@naver.com>
2023-09-08 15:43:58 +0900

    📌refactor: 사용자 회원가입 DTO 수정

--- backend/src/main/java/com/dream/backend/service/user/dto/JoinUserDto.java
+++ backend/src/main/java/com/dream/backend/service/user/dto/JoinUserDto.java
@@ -35,6 +35,7 @@ public class JoinUserDto {
 //        여기에 regionKey 에 알맞는 region 객체를 찾아오고 싶음.
         Optional<Region> regionOptional = regionRepository.findById(regionKey);
         Region region = regionOptional.orElseThrow(() -> new NoSuchElementException("Region not found for regionKey: " + regionKey));
+        System.out.println("regionKey: "+region.getId());
         return User.builder()
                 .name(this.name)
                 .email(this.email)
