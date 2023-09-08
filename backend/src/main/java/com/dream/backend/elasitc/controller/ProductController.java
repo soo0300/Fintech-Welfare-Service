58ff99ae919f4c947c9eda098e1e1a600cb9843f
kunho <didrjsgh11@naver.com>
2023-09-08 14:08:42 +0900

    ✨feat: writed Elasticsearch Rest API for Spring

---
+++ backend/src/main/java/com/dream/backend/elasitc/controller/ProductController.java
@@ -0,0 +1,21 @@
+//package com.dream.backend.elasitc.controller;
+//
+//import com.dream.backend.elasitc.entity.Product;
+//import com.dream.backend.elasitc.service.ProductService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/apis")
+//public class ProductController {
+//
+//    @Autowired
+//    private ProductService productService;
+//
+//    @GetMapping("/findAll")
+//    Iterable<Product> findAll() {
+//        return productService.getProducts();
+//    }
+//}
---
+++ backend/src/main/java/com/dream/backend/elasitc/domain/UserDocument.java
@@ -0,0 +1,36 @@
+package com.dream.backend.elasitc.domain;
+
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.time.LocalDateTime;
+
+@Document(indexName = "user_index")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Setting(settingPath = "elastic/es-setting.json")
+@Mapping(mappingPath = "elastic/es-mapping.json")
+public class UserDocument {
+
+    private Long id;
+    private String name;
+    private Long age;
+    private String korean;
+    private String english;
+
+    @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "uuuu-MM-dd'T'HH:mm:ss")
+    private LocalDateTime created;
+
+//    public static UserDocument of(RequestUserSaveDto dto) {
+//        return UserDocument.builder()
+//            .id(dto.getId())
+//            .name(dto.getName())
+//            .age(dto.getAge())
+//            .korean(dto.getKorean())
+//            .english(dto.getEnglish())
+//            .created(LocalDateTime.now())
+//            .build();
+//    }
+}
