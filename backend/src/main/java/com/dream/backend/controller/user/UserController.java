6f2cf83b41f2bd4fce2858bac39cdc9cc253db56
김수진 <soo0300@naver.com>
2023-09-11 10:43:35 +0900

    📌refactor: 코드 리팩토링

--- backend/src/main/java/com/dream/backend/controller/user/UserController.java
+++ backend/src/main/java/com/dream/backend/controller/user/UserController.java
@@ -8,40 +8,29 @@ import com.dream.backend.service.user.UserService;
 import com.dream.backend.service.user.dto.JoinUserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RequiredArgsConstructor
-@Controller
+@RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+
     @PostMapping("/register")
     public ApiResponse<Long> joinUser(JoinUserRequest request){
         JoinUserDto dto = request.toDto();
          Long id = userService.joinUser(dto);
-        return ApiResponse.ok(id);
+        return ApiResponse.ok(1L);
     }
-
-//    @GetMapping("/all/{user_id}")
-//    public ApiResponse<UserResponse> getUserInfo(@PathVariable Long user_id) {
-//        User user = userService.getUserInfo(user_id);
-//        UserResponse userResponse = user.toResponse(user);
-//        return ApiResponse.ok(userResponse);
-//    }
 
     @GetMapping("/fund/{user_id}")
-    public ApiResponse<UserFundResponse> getUserFund(@PathVariable Long user_id) {
+    public UserFundResponse getUserFund(@PathVariable Long user_id) {
         Optional<User> user = userService.getUserFund(user_id);
         UserFundResponse userResponse = user.get().toFundResponse(user);
-        return ApiResponse.ok(userResponse);
+        return userResponse;
     }
-
 
 }
