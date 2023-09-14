dc2c441e0ead92a5068b38490cc32666c6598999
김수진 <soo0300@naver.com>
2023-09-14 16:23:21 +0900

    📌refactor: 자격조건 엔티티 & 리포지토리 리팩토링

--- backend/src/main/java/com/dream/backend/domain/qualification/Qualification.java
+++ backend/src/main/java/com/dream/backend/domain/qualification/Qualification.java
@@ -1,31 +1,33 @@
-//package com.dream.backend.domain.qualification;
-//
-//import com.dream.backend.domain.region.Region;
-//import com.dream.backend.domain.welfare.Welfare;
-//import lombok.Getter;
-//
-//import javax.persistence.*;
-//import java.io.Serializable;
-//
-//
-//@Entity
-//@Getter
+package com.dream.backend.domain.qualification;
+
+import com.dream.backend.domain.region.Region;
+import com.dream.backend.domain.welfare.Welfare;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Getter
 //@Embeddable
-////extend serializable 생략
-//public class Qualification {
-//    @Id
+//extend serializable 생략
+public class Qualification {
+    @Id
 //    @OneToOne
 //    @JoinColumn(name = "welfare_id")
 //    private Welfare welfare;
-//
-//    @OneToOne
-//    @JoinColumn(name = "region_key")
-//    private Region region;
-//
-//    @Column
-//    private int age;
-//
-//    @Column
-//    private int end_year;
-//
-//}
+    @Column(name = "welfare_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "region_key")
+    private Region region;
+
+    @Column
+    private int age;
+
+    @Column
+    private int end_year;
+
+}
