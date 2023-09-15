7f75ad1e278bd0a3423c70a24c3ae41b25d82e8b
김수진 <soo0300@naver.com>
2023-09-15 14:20:52 +0900

    Revert "✨feat: 회원가입 시 자동 등록 기능 성공[리포지토리 작성]"
    This reverts commit c23b963fbffd573fbc90ba4ff97be9b5713ab55b.

--- backend/src/main/java/com/dream/backend/domain/qualification/repository/QualificationRepository.java
+++ backend/src/main/java/com/dream/backend/domain/qualification/repository/QualificationRepository.java
@@ -2,14 +2,8 @@ package com.dream.backend.domain.qualification.repository;
 
 import com.dream.backend.domain.qualification.Qualification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface QualificationRepository extends JpaRepository<Qualification, Long> {
-    @Query("SELECT q FROM Qualification q WHERE q.region.id = :regionKey AND q.age > :age")
-    List<Qualification> findQualificationsByRegionKeyAndAge(@Param("regionKey") Long regionKey, @Param("age") int age);
+public interface QualificationRepository extends JpaRepository<Qualification,Long> {
 }
