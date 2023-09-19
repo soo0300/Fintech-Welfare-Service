a2766bd56a2a1d92caf5faf0326b58a06084ef8e
daehyuk <sdh5645@gmail.com>
2023-09-19 16:44:38 +0900

    🌈style: 'body전체수정'

--- frontend/src/views/MyPage.jsx
+++ frontend/src/views/MyPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Logo from "../components/Logo/Logo";
+import styled from "styled-components";
+
+const GraphBox = styled.div`
+  width: 10vh;
+  height: 100px;
+  border: 1px solid;
+`;
 
 function MyPage() {
   return (
     <>
       <Logo />
+      <GraphBox></GraphBox>
     </>
   );
 }
