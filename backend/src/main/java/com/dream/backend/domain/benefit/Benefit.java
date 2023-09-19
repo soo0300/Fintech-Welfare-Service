aa7711387b82911be440bfa7ab4227e1e45ca436
김수진 <soo0300@naver.com>
2023-09-19 15:26:46 +0900

    ✨feat: 사용자 복지 카드 엔티티 클래스 재설계

--- backend/src/main/java/com/dream/backend/domain/benefit/Benefit.java
+++ backend/src/main/java/com/dream/backend/domain/benefit/Benefit.java
@@ -15,24 +15,33 @@ import javax.persistence.*;
 public class Benefit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="benefit_id", nullable = false, updatable = false)
+    @Column(name = "benefit_id", nullable = false, updatable = false)
     private Long id;
 
     @OneToOne
-    @JoinColumn(name="user_id", nullable = false, updatable = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
     @OneToOne
-    @JoinColumn(name="welfare_id", nullable = false, updatable = false)
+    @JoinColumn(name = "welfare_id", nullable = false, updatable = false)
     private Welfare welfare;
 
+    @Column
     private int status;
 
+    @Column
+    private int benefit_cnt;
+
+    @Column
+    private boolean is_confirmed;
+
     @Builder
-    public Benefit(Long id, User user, Welfare welfare, int status) {
+    public Benefit(Long id, User user, Welfare welfare, int status, int benefit_cnt, boolean is_confirmed) {
         this.id = id;
         this.user = user;
         this.welfare = welfare;
         this.status = status;
+        this.benefit_cnt = benefit_cnt;
+        this.is_confirmed = is_confirmed;
     }
 }
