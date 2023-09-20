c5203d9b0b5421167d92a432f9d71f26dc491f21
daehyuk <sdh5645@gmail.com>
2023-09-20 17:59:43 +0900

    🌈style: 마이페이지 전체css완성

--- frontend/src/views/MyPage.jsx
+++ frontend/src/views/MyPage.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import Logo from "../components/Logo/Logo";
 import styled from "styled-components";
+import MyData from "../components/mydata/MyData";
+import Nav from "../components/Nav/Nav";
 
-const GraphBox = styled.div`
-  width: 10vh;
-  height: 100px;
-  border: 1px solid;
+const Header = styled.div`
+  width: 100%;
+  height: 70px;
+`;
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 function MyPage() {
   return (
     <>
-      <Logo />
-      <GraphBox></GraphBox>
+      <Header>
+        <Logo></Logo>
+      </Header>
+      <Content>
+        <MyData></MyData>
+      </Content>
+      <Nav></Nav>
     </>
   );
 }
