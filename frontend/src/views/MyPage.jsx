5c2a160ee82184f033aec562ea38b8511885de45
lIIIlllIII <90883458+mko56@users.noreply.github.com>
2023-09-21 01:24:35 +0900

    🚨fix: 챗봇 키보드안내려가는 버그수정

--- frontend/src/views/MyPage.jsx
+++ frontend/src/views/MyPage.jsx
@@ -4,12 +4,25 @@ import styled from "styled-components";
 import MyData from "../components/mydata/MyData";
 import Nav from "../components/Nav/Nav";
 
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { ReactComponent as AvartarIcon } from "../assets/img/Avartar.svg";
+
 const Header = styled.div`
-  width: 100%;
+  width: 90%;
   height: 70px;
+  display: flex;
+  align-items: center;
 `;
 const Content = styled.div`
-  width: 100%;
+  width: 90%;
   height: 100%;
   overflow-y: scroll;
   overflow-x: hidden;
@@ -18,11 +31,89 @@ const Content = styled.div`
   }
 `;
 
+const Info = styled.div`
+  width: 20%;
+  height: 40px;
+  display: flex;
+  border: 1px solid;
+`;
+
 function MyPage() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <Header>
         <Logo></Logo>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <AvartarIcon />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 20,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={handleClose}>
+            <Avatar /> 내정보
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            로그아웃
+          </MenuItem>
+        </Menu>
       </Header>
       <Content>
         <MyData></MyData>
