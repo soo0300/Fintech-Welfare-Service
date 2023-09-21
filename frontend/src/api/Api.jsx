2fa5f693c45f262d747616d33f32ae1416b692c9
홍주영 <darkssg@naver.com>
2023-09-21 11:14:04 +0900

    📌refactor: signup API 수정

--- frontend/src/api/Api.jsx
+++ frontend/src/api/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const baseAxios = axios.create({
-  baseURL: "http://j9c209.p.ssafy.io:8085",
+  baseURL: "http://j9c209.p.ssafy.io:8090",
   //   baseURL: "https://i9c110.p.ssafy.io/api/",
 });
