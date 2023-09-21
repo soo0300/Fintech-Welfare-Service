b42bad02002dcb4fdbc663101ec12da963aea5dd
김수진 <soo0300@naver.com>
2023-09-21 10:38:01 +0900

    ✨feat: 로그인 예외 처리

--- backend/src/main/java/com/dream/backend/service/user/LoginService.java
+++ backend/src/main/java/com/dream/backend/service/user/LoginService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -17,8 +18,11 @@ public class LoginService {
 
     public Long loginUser(String email, String pwd) {
         Optional<User> user = Optional.ofNullable(userRepository.findByEmailAndPassword(email, pwd));
-        Long id = user.get().getId();
-        return id;
-
+        if (user.isPresent()) {
+            return user.get().getId();
+        } else {
+            // 예외 처리: 유저를 찾을 수 없는 경우
+            throw new NoSuchElementException("아이디와 비밀번호를 확인하세요");
+        }
     }
 }
