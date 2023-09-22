12c9e51b40346414b4d6444ed0fbba7c1f73b367
kunho <didrjsgh11@naver.com>
2023-09-22 10:24:02 +0900

    ✨feat: 한국어 형태소 분석기 강화(필터링 추가)

--- backend/src/main/java/com/dream/backend/elasitc/service/WelfareInfoService.java
+++ backend/src/main/java/com/dream/backend/elasitc/service/WelfareInfoService.java
@@ -145,6 +145,42 @@ public class WelfareInfoService {
         return null;
     }
 
+    public List<WelfareInfo> complexSearchDocument(String must, String query) {
+        List<WelfareInfo> results = new ArrayList<>();
+        SearchResponse<WelfareInfo> response;
+
+        try {
+            response = esClient.search(s -> s
+                    .index("welfare_info")
+                    .query(q -> q
+                            .bool(b -> b
+                                    .must(m -> m
+                                            .match(t -> t
+                                                    .field("keywords")
+                                                    .query(must)))
+                                    .should(sd -> sd
+                                            .match(t -> t
+                                                    .field("keywords")
+                                                    .query(query))))),
+                    WelfareInfo.class
+            );
+
+            List<Hit<WelfareInfo>> hits = response.hits().hits();
+            for(Hit<WelfareInfo> hit: hits) {
+                WelfareInfo welfareInfo = hit.source();
+                welfareInfo.setScore(hit.score());
+                results.add(welfareInfo);
+            }
+
+            return results;
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     public List<WelfareInfo> searchReliableDocument(String words) {
         List<WelfareInfo> results = new ArrayList<>();
         SearchResponse<WelfareInfo> response;
@@ -196,6 +232,7 @@ public class WelfareInfoService {
 //                    ContentType.APPLICATION_JSON
 //            ));
 
+//            필터 추가: 어미, 감탄사, 조사, 의존명사, 대명사, 일반부사, 접속부사, 동사, 동사 파생 접미사, 보조 용언
             AnalyzeRequest request = AnalyzeRequest.withIndexAnalyzer("korean_analyzer", "nori_analyzer", text);
             AnalyzeResponse response = client.indices().analyze(request, RequestOptions.DEFAULT);
 
