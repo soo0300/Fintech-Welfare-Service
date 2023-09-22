bc4b3f5a68ab8f667df90b466d80b2d4d3d7b5bf
daehyuk <sdh5645@gmail.com>
2023-09-22 17:48:27 +0900

    ✨feat: 마이페이지 비밀번호변경추가

--- frontend/src/components/mydata/MyInfo.jsx
+++ frontend/src/components/mydata/MyInfo.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Button, TextField } from "@mui/material";
 import { ChangePassword, GetUser } from "../../api/mypage/UserInformation";
-import { width } from "@mui/system";
 import ChangePwd from "./ChangePwd";
 
 //정보박스
@@ -27,7 +26,7 @@ const TextBox = styled.div`
   border-bottom: 2px solid lightgray;
 `;
 
-const InfoTextBox = styled.p`
+const InfoTextBox = styled.div`
   width: 90%;
   display: flex;
   justify-content: space-between;
@@ -55,16 +54,12 @@ function MyInfo() {
     age: null,
   });
 
-  //영문,숫자,특수기호를 포함한 8자리 이상, 15자리 이하
-  const passwordRegEx = /^(?=.*[a-zA-Z])(?=.*[!@#$%^+=-])(?=.*[0-9]).{8,15}$/;
-  const [isPasswordValid, setPasswordValid] = useState(false);
-
   const backMypage = () => {
     window.location.reload();
   };
 
   const userInfo = async () => {
-    const res = await GetUser(sessionStorage.getItem("id"));
+    const res = await GetUser(localStorage.getItem("id"));
     setInfo(res.data);
   };
 
@@ -85,23 +80,29 @@ function MyInfo() {
           <p>{info.name}님의 정보</p>
           <Button onClick={backMypage}>뒤로 돌아가기</Button>
         </TextBox>
-        <InfoTextBox>이메일 : {info.email}</InfoTextBox>
         <InfoTextBox>
-          비밀번호 : {info.password}
-          <Button onClick={() => changeMode(1)}>비밀번호변경</Button>
+          {info.email}
+          <Button onClick={() => changeMode(1)} sx={{ padding: 0 }}>
+            비밀번호변경
+          </Button>
         </InfoTextBox>
+
         <InfoTextBox>
           거주지 : {info.regionKey}
-          <Button onClick={() => changeMode(2)}>거주지 변경</Button>
+          <Button onClick={() => changeMode(2)} sx={{ padding: 0 }}>
+            거주지 변경
+          </Button>
         </InfoTextBox>
         <InfoTextBox>생년월일 : {info.age}</InfoTextBox>
         <InfoTextBox>
           보호종료일 : 보호종료일
-          <Button onClick={() => changeMode(3)}>보호종료일 변경</Button>
+          <Button onClick={() => changeMode(3)} sx={{ padding: 0 }}>
+            보호종료일 변경
+          </Button>
         </InfoTextBox>
       </InfoBox>
       {mode === 0 && null}
-      {mode === 1 && <ChangePwd />}
+      {mode === 1 && <ChangePwd curPwd={info.password} />}
       {mode === 2 && (
         <InfoBox>
           <TextBox>
