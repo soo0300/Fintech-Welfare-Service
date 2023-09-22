045a63adedb558adcee6a7baf2233783c22ced3d
김수진 <soo0300@naver.com>
2023-09-22 14:22:08 +0900

    📌refactor: [마이데이터 반영]toEntity() 함수 수정

--- backend/src/main/java/com/dream/backend/service/user/dto/JoinUserDto.java
+++ backend/src/main/java/com/dream/backend/service/user/dto/JoinUserDto.java
@@ -25,11 +25,10 @@ public class JoinUserDto {
     private LocalDateTime endDate;
     private boolean isEnded;
     private LocalDateTime createdDate;
-    private boolean myData;
 
 
     @Builder
-    public JoinUserDto(String name, String email, String password, int residenceInfo, Long regionKey, LocalDateTime endDate, LocalDateTime createdDate, boolean isEnded, boolean myData) {
+    public JoinUserDto(String name, String email, String password, int residenceInfo, Long regionKey, LocalDateTime endDate, LocalDateTime createdDate, boolean isEnded) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -38,12 +37,11 @@ public class JoinUserDto {
         this.endDate = endDate;
         this.createdDate = createdDate;
         this.isEnded = isEnded;
-        this.myData = myData;
     }
 
-    public User toEntity(Optional<Region> region) {
+    public User toEntity(Optional<Region> region, boolean type) {
 //        여기에 regionKey 에 알맞는 region 객체를 찾아오고 싶음.
-        System.out.println("regionKey: "+this.regionKey);
+        System.out.println("regionKey: "+region.get().getId());
         //region 객체가 안가져와짐
         System.out.print("find: "+region.get().getId());
         return User.builder()
@@ -55,7 +53,7 @@ public class JoinUserDto {
                 .is_ended(this.isEnded)
                 .end_date(this.endDate)
                 .created_date(this.createdDate)
-                .my_data(this.myData)
+                .my_data(type)
                 .build();
     }
 }
