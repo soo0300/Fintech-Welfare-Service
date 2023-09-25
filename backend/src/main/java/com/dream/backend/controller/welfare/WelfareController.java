6178c2846c73c28a24a0b1de8bd3935ac74cc334
김수진 <soo0300@naver.com>
2023-09-25 12:07:48 +0900

    🌈style: 주석

--- backend/src/main/java/com/dream/backend/controller/welfare/WelfareController.java
+++ backend/src/main/java/com/dream/backend/controller/welfare/WelfareController.java
@@ -20,13 +20,13 @@ public class WelfareController {
     private final WelfareService welfareService;
     private final QualificationService qualificationService;
 
-    //전체 복지 정보 조회
+    //[전체 복지 정보 조회]
     @GetMapping("/all")
     public List<WelfareResponse> getAllWelfare() {
         return welfareService.getAllWelfare();
     }
 
-    //[사용자와 관계없 없이 지역 분류]
+    //[사용자와 관계없이 지역 분류]
     @GetMapping("/{regionKey}")
     public List<WelfareResponse> getRegionWelfare(@PathVariable Long regionKey) {
         List<WelfareResponse> response = new ArrayList<>();
