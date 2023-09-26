80db6e2723ac19b5b524af055ef50a2978865484
홍주영 <darkssg@naver.com>
2023-09-26 16:06:18 +0900

    📌refactor: API url변경

--- frontend/src/api/mypage/User.jsx
+++ frontend/src/api/mypage/User.jsx
@@ -2,7 +2,8 @@ import { baseAxios } from "../Api";
 
 export async function Signup(props) {
   try {
-    const res = await baseAxios.post(`user/signup/${props.myData}`, {
+    const curStatus = props.myData ? "1" : "0";
+    const res = await baseAxios.post(`user/signup/${curStatus}`, {
       name: props.name,
       email: props.email,
       password: props.password,
