7dfeb710cec8fd50203c818b9e854df2e9e8deb7
kunho <didrjsgh11@naver.com>
2023-10-04 17:28:13 +0900

    📌refactor: ApiResponse 추가

--- backend/src/main/java/com/dream/backend/elasitc/controller/ProductController.java
+++ backend/src/main/java/com/dream/backend/elasitc/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.dream.backend.elasitc.controller;
 
 
+import com.dream.backend.controller.ApiResponse;
 import com.dream.backend.elasitc.service.WelfareInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,12 @@ public class ProductController {
     private final WelfareInfoService welfareInfoService;
 
     @GetMapping("/{korean_text}")
-    public String getToken(@PathVariable("korean_text") String text) throws IOException {
+    public ApiResponse<String> getToken(@PathVariable("korean_text") String text) throws IOException {
 
         welfareInfoService.setClient();
         String result = welfareInfoService.tokenized(text);
         welfareInfoService.closeAllClient();
 
-        return result;
+        return ApiResponse.ok(result);
     }
 }
