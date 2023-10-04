e4e3c0e91dcb35808b5b34fa1335a4fa0128b24a
kunho <didrjsgh11@naver.com>
2023-10-04 11:01:10 +0900

    📌refactor: StatusObject 위치 변경, Post Request에 대한 Return 형식을 StatusObject으로 변환

--- backend/src/main/java/com/dream/backend/elasitc/controller/Status/StatusObject.java
+++
@@ -1,9 +0,0 @@
-package com.dream.backend.elasitc.controller.Status;
-
-public class StatusObject {
-
-    private int code;
-    private String status;
-    private String message;
-    private Object data;
-}
