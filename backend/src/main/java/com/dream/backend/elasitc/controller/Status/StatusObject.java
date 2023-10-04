b071448b7c45ed9b7441372b26064748d4423ca2
kunho <didrjsgh11@naver.com>
2023-10-04 10:34:23 +0900

    📌refactor: Elasticsearch Controller 부분에 대한 Status 반환 Json 객체 작성

---
+++ backend/src/main/java/com/dream/backend/elasitc/controller/Status/StatusObject.java
@@ -0,0 +1,9 @@
+package com.dream.backend.elasitc.controller.Status;
+
+public class StatusObject {
+
+    private int code;
+    private String status;
+    private String message;
+    private Object data;
+}
